919d968af439991c2d5f28d96e8213e460ea0170
scannella <scannella@bishopireton.org>
2017-03-07 10:41:36 -0500

    might leave class

--- app/src/main/java/com/scannella/quarter3final/MainActivity.java
+++ app/src/main/java/com/scannella/quarter3final/MainActivity.java
@@ -1,10 +1,12 @@
 package com.scannella.quarter3final;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +16,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     //final Handler handler = new Handler();
+    Display display = getWindowManager().getDefaultDisplay();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +25,27 @@ public class MainActivity extends AppCompatActivity {
 
         final Button button = (Button) findViewById(R.id.btnClick);
 
+        Point size = new Point();
+        display.getSize(size);
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DisplayMetrics metrics = new DisplayMetrics();
-                getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
-                int newX = new Random().nextInt(metrics.heightPixels - 50);     //number from 0-
-                int newY = new Random().nextInt(metrics.widthPixels - 50);
 
-                button.setX(newX + 50);
-                button.setY(newY + 50);
+
+
+                //int newX = new Random().nextInt(metrics.heightPixels - 50);     //number from 0-
+                //int newY = new Random().nextInt(metrics.widthPixels - 50);
+
+                int newX = (int)(Math.random() * display.getWidth() + 1);
+                int newY = (int)(Math.random() * display.getHeight() + 1);
+
+
+                button.setX(newX);
+                button.setY(newY);
             }
         });
 
