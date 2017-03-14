13c4db9483ed38a789bf59f4012c7c9b03555ca9
KsMomOf2 <kellyt@bishopireton.org>
2017-03-13 20:43:04 -0400

    Adjusted the random movement of the button to use the relative layout instead of the display Changed the way the handler is called to move the button randomly

--- app/src/main/java/com/scannella/quarter3final/MainActivity.java
+++ app/src/main/java/com/scannella/quarter3final/MainActivity.java
@@ -1,5 +1,6 @@
 package com.scannella.quarter3final;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Point;
@@ -8,68 +9,73 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     //final Handler handler = new Handler();
-
+    private Button button;
+    private Activity thisName;
+    private int maxWidth;
+    private int maxHeight;
+    private RelativeLayout relativeLayout;
+    private View myView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = (Button) findViewById(R.id.btnClick);
-
+        button = (Button) findViewById(R.id.btnClick);
+        relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
         final MediaPlayer play = MediaPlayer.create(this,R.raw.exclamation);
 
+        thisName = this;
 
+        relativeLayout.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+             @Override
+             public void onLayoutChange(View v, int l, int r, int t, int b, int oldL, int oldR, int oldT, int OldB){
+                 //If l, r, t, and b are all 0 then the layout is not ready - just return
+                 if (l == 0 && r == 0 && t == 0 && b == 0) return;
+                 Log.d("Points","R: ("+relativeLayout.getWidth()+", "+ relativeLayout.getHeight()+")");
+                 Log.d("Points","B: " + button.getWidth()+ " -- " + button.getHeight());
+                 maxHeight = relativeLayout.getHeight() - button.getHeight() - relativeLayout.getPaddingTop() - relativeLayout.getPaddingBottom()+ 1;
+                 maxWidth = relativeLayout.getWidth() - button.getWidth() - relativeLayout.getPaddingLeft() - relativeLayout.getPaddingRight()+ 1;
+                 Log.d("Points","M: " + maxWidth + ", " + maxHeight);
+             }
+         });
 
-        button.setOnClickListener(new View.OnClickListener() {
+         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                //Display display = getWindowManager().getDefaultDisplay();
-
-
-                //int newX = new Random().nextInt(metrics.heightPixels - 50);     //number from 0-
-                //int newY = new Random().nextInt(metrics.widthPixels - 50);
-
-                //int newX = (int)(Math.random() * display.getWidth() + 1);
-                //int newY = (int)(Math.random() * display.getHeight() + 1);
-
-
-                //button.setX();
-                //button.setY();
-
-
+                setButtonRandomPosition();
                 play.start();
-
             }
         });
-
-        /*handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                *//*Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(intent);*//*
-
-            }
-        }, 5000);*/
-
-
+        handler.postDelayed(runnable,3000);
     }
 
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable(){
+        @Override
+        public void run() {
+            setButtonRandomPosition();
+            handler.postDelayed(runnable, 3000);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,46 +84,34 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-
         if (id == R.id.itmSettings) {
-
             Intent intent = new Intent(MainActivity.this, Settings.class);
             startActivity(intent);
-
         }
-
-
-
         return super.onOptionsItemSelected(item);
     }
 
-    /*private void setButtonRandomPosition(Button button){
-        int newX = new Random().nextInt(getDisplaySize(this).x);     //number from 0-
-        int newY = new Random().nextInt(getDisplaySize(this).y);
+    private void setButtonRandomPosition(){
+                int newX = (int)(Math.random() * maxWidth);
+                int newY = (int)(Math.random() * maxHeight);
+                Log.d("Points","("+newX+","+newY+")");
+                button.setX(newX);
+                button.setY(newY);
+    }
 
-        button.setX(newX);
-        button.setY(newY);
-    }*/
-
-    /*private void startRandomButton(Button button) {
+    private void startRandomButton() {
         Timer timer = new Timer();
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                setButtonRandomPosition(button);
+                setButtonRandomPosition();
             }
-        }, 0, 1000);//Update button every second
+        }, 0, 3000);//Update button every three seconds
     }
-
-*/
-
-
 }
